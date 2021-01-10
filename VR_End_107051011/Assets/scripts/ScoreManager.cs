160790be4bee6e96c30a493a8b5d967d24d8a7b3
b50905du <71679904+b50905du@users.noreply.github.com>
2021-01-10 18:34:34 +0800

    期末完成

---
+++ VR_End_107051011/Assets/scripts/ScoreManager.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour
+{
+    [Header("分數介面")]
+    public Text text;
+    [Header("分數")]
+    public int score;
+    [Header("得分")]
+    public int scoreIn = 1;
+    [Header("進球音效")]
+    public AudioClip soundIn;
+
+    private AudioSource aud;
+
+    private void Awake()
+    {
+        aud = GetComponent<AudioSource>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "圈圈")
+        {
+            AddScore();
+        }
+    }
+    private void AddScore()
+    {
+        score += scoreIn;
+        text.text = score.ToString();
+        aud.PlayOneShot(soundIn, Random.Range(1f, 2f));
+    }
+
+
+}
